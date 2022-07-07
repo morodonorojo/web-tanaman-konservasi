1a9318e016cbc5e6934e505c454e473fefb683bb
aldoarya <arudo.saka@gmail.com>
2022-07-07 23:22:23 +0700

    feat: Added prismic and PlantData Custom Type

---
+++ .slicemachine/assets/slices/ImageGallery/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/ImageGallery';
+
+export default {
+  title: 'slices/ImageGallery'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"image_gallery","items":[{"Image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1551739440-5dd934d3a94a?w=900&h=500&fit=crop"}},{"Image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1448932223592-d1fc686e76ea?w=900&h=500&fit=crop"}},{"Image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1491975474562-1f4e30bc9468?w=900&h=500&fit=crop"}}],"primary":{},"id":"_Default"}} />
+_Default.storyName = 'Default'
