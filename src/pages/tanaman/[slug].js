6fdf88428f435aef81bfcf701f7708f26b17a6d7
aldoarya <arudo.saka@gmail.com>
2022-07-09 00:40:23 +0700

    feat: Filtering functionality for Plant Explanation

--- src/pages/tanaman/[slug].js
+++ src/pages/tanaman/[slug].js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
+import clsx from "clsx";
 
 import BackArrowHeader from "../../components/BackArrowHeader";
 import Carousel from "../../components/Carousel";
 import { MainButton } from "../../components/Button";
 
-const DetailTanaman = () => {
+const DetailTanaman = ({...props}) => {
   const [activeCategory, setActiveCategory] = useState("peran");
 
   return (
-    <section className="mx-auto w-full xl:container">
+    <section className="mx-auto w-full xl:container" {...props}>
       <div className="mt-3 px-3">
         <BackArrowHeader />
       </div>
@@ -16,7 +17,7 @@ const DetailTanaman = () => {
         <Carousel />
       </div>
       <div className="plant-detail min-h-screen w-full rounded-tr-2xl rounded-tl-2xl bg-mutedgray-25 p-3">
-        <div className="category-buttons w-full child:mx-3">
+        <div className="category-buttons w-full child:mx-1">
           <MainButton
             data-category="peran"
             onClick={(e) => setActiveCategory(e.target.dataset.category)}
@@ -38,6 +39,56 @@ const DetailTanaman = () => {
           >
             Persebaran
           </MainButton>
+        </div>
+        <div className="explanation relative font-body text-sm text-justify mt-4">
+          <div
+            data-category="peran"
+            className={clsx(
+              "absolute w-full",
+              activeCategory === "peran"
+                ? "left-0 transition-all opacity-100"
+                : "-left-[5555px] transition-all opacity-0"
+            )}
+          >
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+              odit delectus ullam reprehenderit placeat sapiente ex animi non
+              ea! Molestiae, quisquam non aspernatur minima iste illum id vitae
+              sed consequuntur?
+            </p>
+          </div>
+          <div
+            data-category="morfologi"
+            className={clsx(
+              "absolute w-full",
+              activeCategory === "morfologi"
+                ? "left-0 transition-all opacity-100"
+                : "-left-[5555px] transition-all opacity-0"
+            )}
+          >
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+              odit delectus ullam reprehenderit placeat sapiente ex animi non
+              ea! Molestiae, quisquam non aspernatur minima iste illum id vitae
+              sed consequuntur?
+            </p>
+          </div>
+          <div
+            data-category="persebaran"
+            className={clsx(
+              "absolute w-full",
+              activeCategory === "persebaran"
+                ? "left-0 transition-all opacity-100"
+                : "-left-[5555px] transition-all opacity-0"
+            )}
+          >
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
+              odit delectus ullam reprehenderit placeat sapiente ex animi non
+              ea! Molestiae, quisquam non aspernatur minima iste illum id vitae
+              sed consequuntur?
+            </p>
+          </div>
         </div>
       </div>
     </section>
