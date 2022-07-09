6797a5ce7d98909dd7403f1639fbf12eb96c0c05
aldoarya <arudo.saka@gmail.com>
2022-07-09 15:50:02 +0700

    fix: Navbar not showing when clicked

--- src/components/Layout/Navbar/index.js
+++ src/components/Layout/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
@@ -7,6 +7,10 @@ import HamburgerMenu from "../../../assets/icon/hamburger-menu.svg";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(isMenuOpen);
+  }, [isMenuOpen]);
 
   return (
     <nav className="sticky top-0 z-50 flex h-14 w-full flex-row items-center justify-between bg-beachbrown-900 p-3 drop-shadow-lg">
@@ -33,8 +37,8 @@ const Navbar = () => {
       </div>
       <div
         className={clsx(
-          "absolute left-[-100%] top-0 mt-14 h-screen w-full bg-beachbrown-500 p-2 transition-all",
-          isMenuOpen && "left-0 transition-all"
+          "absolute top-0 mt-14 h-screen w-full bg-beachbrown-500 p-2 transition-all",
+          isMenuOpen ? "left-0 transition-all" : "left-[-100%]"
         )}
       >
         <ul className="font-body child:mb-1 child:flex child:child:h-full child:child:w-full child:cursor-pointer child:rounded-lg child:p-2 child:text-white child-hover:bg-white/20">
